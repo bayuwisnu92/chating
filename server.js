488ecc473e8e0e44d8a3375e13d87f7e8330a255
yahya <yza@voxteneo.com>
2024-05-13 17:17:51 +0700

    fix

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const chatRoutes = require('./app/routes/chatRoutes');
+const chatController = require('./app/controllers/chatController');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const chatController = require('./app/controllers/chatController');
 
 // Gunakan middleware untuk menangani rute chat
 app.use('/', chatRoutes);
